83ac8a05f67f969cafdac9a2e3db30f62fe4f4f4
Yan Shytyk <shytyk.develop@gmail.com>
2026-02-11 19:23:47 +0100

    Fix internal server issue

--- database.py
+++ database.py
@@ -3,7 +3,18 @@ from sqlalchemy import Column, BigInteger, String, Integer, select, delete
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+raw_url = os.getenv("DATABASE_URL")
+
+
+if raw_url:
+    if raw_url.startswith("postgres://"):
+        DATABASE_URL = raw_url.replace("postgres://", "postgresql+asyncpg://", 1)
+    elif raw_url.startswith("postgresql://"):
+        DATABASE_URL = raw_url.replace("postgresql://", "postgresql+asyncpg://", 1)
+    else:
+        DATABASE_URL = raw_url
+else:
+    DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 
@@ -19,8 +30,9 @@ class Favorite(Base):
     ticker = Column(String)
 
 async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    if engine:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
 async def add_favorite(user_id: int, ticker: str):
     async with async_session() as session:
